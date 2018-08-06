608e861ebeba3dd604d46d9a4825ed84c605b795
akakou <s16003@tokyo.kosen-ac.jp>
2018-08-06 20:50:02 +0900

    [add] Making Scanner Struct List
    * Making `Scanner` struct and impl
    * Push  scanner struct which has scanner config file and process to vector

--- src/scan.rs
+++ src/scan.rs
@@ -1,8 +1,30 @@
 use std::vec::Vec;
 use std::fs;
+use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 extern crate config;
+
+
+struct Scanner {
+    name: String,
+    path: String,
+    config: config::Config,
+    process: Command
+}
+
+impl Scanner {
+    fn info_string(&mut self) -> String {
+        let output =  &self.process.output().unwrap().stdout;
+
+        let res = output.iter().map(|&s| s as char).collect::<String>();
+        let output: String = String::from_utf8(output.to_vec()).unwrap();
+
+        return "name: ".to_string() + &self.name + "\n"
+            + "output: " + &output.to_string();
+    }
+}
 
 
 /// Search files which named `file_names` 
@@ -36,7 +58,14 @@ fn search_files(files_paths: &mut Vec<PathBuf>, directory_path: PathBuf) {
 
 /// Using signatures and scanner to scan target.
 pub fn scan() {
-    /* get scanner */
+    /* Get scanner */
+    let mut scanners: Vec<Scanner> = Vec::new();
+
+    // get current directory
+    let unwraped_current_path = env::current_dir().unwrap();
+    let current_path = unwraped_current_path.to_str().unwrap();
+
+    // read config
     let mut settings = config::Config::default();
     match settings.merge(config::File::with_name("conf/Scanner")) {
         Ok(_) => {},
@@ -48,6 +77,7 @@ pub fn scan() {
         }
     }
 
+    // get scanner names
     let scanners_names = match settings.get_array("scanners") {
         Ok(_scanners) => _scanners,
         Err(_err) => {
@@ -58,10 +88,59 @@ pub fn scan() {
         }
     };
 
-    // print signatures
-    println!("[Scanner Name]");
-    for scanner_name in scanners_names {
-        println!("Name: {}", scanner_name);
+    // make scanner struct
+    for unwraped_scanner_name in scanners_names {
+        // get scanner path
+        let mut scanner_name = match unwraped_scanner_name.into_str() {
+            Ok(_scanner) =>  _scanner,
+            Err(_err) => {
+                println!("[Err] Scanner Name Error\n\
+                Please check is `scanners` in conf/Scanner.toml correct.\n\n\
+                {}", _err);
+                continue;
+            }
+        };
+        
+        // get scanner path
+        let scanner_path = "scanner/".to_string() + &scanner_name;
+
+        // get config
+        let config_path = scanner_path.to_string() + "/Settings";
+        let mut scanner_config = config::Config::default();
+        match settings.merge(config::File::with_name(config_path.as_str())) {
+            Ok(_) => {},
+            Err(_err) => {
+                println!("[Err] Scanner Config Error\n\
+                    Please check {}.toml exists.\n\n\
+                    {}", config_path, _err);
+                continue;
+            }
+        };
+
+        // get excutable file path
+        let executable_path = match settings.get_str("excutable_file") {
+            Ok(_excutable) => current_path.to_string() + "/" + &scanner_path.as_str().to_string() + "/" + &_excutable.to_string(),
+            Err(_err) => {
+                println!("[Err] Setting File Error\n\
+                    Please check {}.toml has `excutable_file` string.\n\n\
+                    {}", config_path, _err);
+                continue;
+            }
+        };
+
+        // make scanner
+        scanners.push(
+            Scanner {
+                name: scanner_name,
+                path: scanner_path,
+                config: scanner_config,
+                process: Command::new(executable_path)
+            }
+        );
+
+        // for debug
+        let mut pop = scanners.pop().unwrap();
+        println!("load:{}", pop.info_string())
     }
 
     /* get signature list */
